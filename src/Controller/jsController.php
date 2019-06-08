2fa890ee4eba1b8eaf3104822e0dab27963db544
vagrant <vagrant@homestead>
2019-06-08 21:20:04 +0000

    Add new BeautyPass page

---
+++ src/Controller/jsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/js")
+ */
+class jsController extends Controller
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render(
+            'raw_Html/index.html',
+            [
+
+            ]
+        );
+    }
+
+}
