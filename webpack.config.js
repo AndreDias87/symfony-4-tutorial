669d8ffcdea3b273e7d2e5a43d45f3c3f2896cfb
AndreDias87 <andre.rato.87@gmail.com>
2019-07-18 21:59:49 -0400

    Disable sass

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // enables Sass/SCSS support
-    .enableSassLoader()
+    //.enableSassLoader()
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
